9c9e98e6376fc3caa9d967c1d160dae775a6d66f
tekuteku11udana <tekuteku11udana888@gmail.com>
2022-06-28 14:34:27 +0900

    Add eslint-plugin-jest configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "standard",
-        "prettier", // 追加。他の設定の上書きを行うために、必ず最後に配置する。
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "standard",
+    "plugin:jest/recommended",
+    "prettier", // 追加。他の設定の上書きを行うために、必ず最後に配置する。
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "jest"],
+  rules: {},
+};
