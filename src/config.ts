a5baf33721f62832c629cf53f5e3f98a58a97924
tekuteku11udana <tekuteku11udana888@gmail.com>
2022-07-14 00:00:44 +0900

    Stop using .env file besides production.

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,27 @@
+const NODE_ENV = process.env.NODE_ENV as "production" | "development" | "test";
+
 const config = {
-  NODE_ENV: process.env.NODE_ENV as "production" | "development" | "test",
+  NODE_ENV,
   server: {
-    PORT: Number(process.env.PORT),
+    PORT: NODE_ENV === "production" ? Number(process.env.PORT) : 3001,
   },
   db: {
-    CONNECTION_URI: process.env.DATABASE_URL as string,
+    CONNECTION_URI:
+      NODE_ENV === "production"
+        ? (process.env.DATABASE_URL as string)
+        : "postgresql://dev:dev@localhost:5431/yjs_playground",
   },
   redis: {
-    CONNECTION_URI: process.env.REDIS_URL as string,
+    CONNECTION_URI:
+      NODE_ENV === "production"
+        ? (process.env.REDIS_URL as string)
+        : "redis://localhost:6379",
   },
   front: {
-    ORIGIN: process.env.FRONT_ORIGIN as string,
+    ORIGIN:
+      NODE_ENV === "production"
+        ? (process.env.FRONT_ORIGIN as string)
+        : "http://localhost:3000",
   },
   SESSION_EXPIRATION_TIME: 5 * 60, // seconds
 };
